eb129e0438d8597ef7a1b580652944d23d5cc48c
Daniel Van Cuylenburg <email@danielvanc.com>
2024-01-23 21:58:05 +0000

    chore: install and configure playwright

---
+++ playwright.config.ts
@@ -0,0 +1,65 @@
+import { defineConfig, devices } from "@playwright/test";
+
+const PORT = process.env.PORT || "3000";
+
+export default defineConfig({
+  testDir: "./tests/e2e",
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: "html",
+  use: {
+    baseURL: `http://localhost:${PORT}/`,
+    trace: "on-first-retry",
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
+
+    {
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
+    },
+
+    {
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
+    },
+
+    /* Test against mobile viewports. */
+    // {
+    //   name: 'Mobile Chrome',
+    //   use: { ...devices['Pixel 5'] },
+    // },
+    // {
+    //   name: 'Mobile Safari',
+    //   use: { ...devices['iPhone 12'] },
+    // },
+
+    /* Test against branded browsers. */
+    // {
+    //   name: 'Microsoft Edge',
+    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    // },
+    // {
+    //   name: 'Google Chrome',
+    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    // },
+  ],
+
+  // webServer: {
+  //   command: process.env.CI ? "npm run start:mocks" : "npm run dev",
+  //   port: Number(PORT),
+  //   reuseExistingServer: !process.env.CI,
+  //   stdout: "pipe",
+  //   stderr: "pipe",
+  //   env: {
+  //     PORT,
+  //   },
+  // },
+});
