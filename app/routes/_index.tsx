0bed53a5dba72564e32ed14abe588c1b00aafb5c
Daniel Van Cuylenburg <email@danielvanc.com>
2024-01-26 20:25:07 +0000

    feat: update text - test staging

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,8 @@
 export default function Index() {
   return (
-    <div className="p-10 w-1/3 text-4xl text-blue-400 font-bold mx-auto">
-      <h1>👋 Welcome to Align.</h1>
+    <div className="p-10 w-1/3 mx-auto">
+      <h1 className="text-4xl text-blue-400 font-bold">👋 Welcome to Align.</h1>
+      <p>Plan your tasks to meet your goals!</p>
     </div>
   );
 }
