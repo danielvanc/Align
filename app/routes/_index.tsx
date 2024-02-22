9889332f835ba8919d85b2053b0f2098403f08ba
Daniel Van Cuylenburg <email@danielvanc.com>
2024-02-22 19:49:38 +0000

    fix: update description

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,7 +2,7 @@ export default function Index() {
   return (
     <div className="mx-auto w-1/3 p-10">
       <h1 className="text-4xl font-bold text-blue-400">Welcome to Align!</h1>
-      <p>Plan your tasks to meet your goals. Ok?</p>
+      <p>Plan your tasks to meet your goals.</p>
     </div>
   );
 }
